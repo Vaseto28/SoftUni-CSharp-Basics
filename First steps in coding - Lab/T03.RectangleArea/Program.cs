4c63eac47e52a2376a25fb180052faa1eb8e21dc
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-24 11:41:32 +0200

    Completed task: T03.RectangleArea

---
+++ First steps in coding - Lab/T03.RectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
