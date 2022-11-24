c88c3251dc35d7eccb82bc1c6a37cb27c42c0969
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-24 11:43:11 +0200

    Completed task: T06.PetShop

---
+++ First steps in coding - Lab/T06.PetShop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace petshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = int.Parse(Console.ReadLine());
+            var cats = int.Parse(Console.ReadLine());
+            var price = dogs * 2.50 + cats * 4;
+            Console.WriteLine(price + " lv.");
+        }
+    }
+}
