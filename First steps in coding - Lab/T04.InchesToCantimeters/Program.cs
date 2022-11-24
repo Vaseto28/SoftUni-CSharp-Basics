071fe968e353ccf4f709c78058fcddb7e459cfba
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-24 11:41:59 +0200

    Completed task: T04.InchesToCantimeters

---
+++ First steps in coding - Lab/T04.InchesToCantimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;T
+
+namespace inches_and_santimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inches = double.Parse(Console.ReadLine());
+            var santimeters = inches * 2.54;
+            Console.WriteLine(santimeters);
+        }
+    }
+}
