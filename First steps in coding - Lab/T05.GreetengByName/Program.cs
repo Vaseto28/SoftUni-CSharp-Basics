82c8cecf63f9a8b55818d5aebf704291097f4ecb
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-24 11:37:28 +0200

    Initial commit

---
+++ First steps in coding - Lab/T05.GreetengByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
