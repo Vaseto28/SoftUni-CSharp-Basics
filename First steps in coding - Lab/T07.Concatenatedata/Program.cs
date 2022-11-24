15c9c80389b5c09e64c1df77a3d79a45ea774d2e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-24 11:43:31 +0200

    Completed task: T07.ConcatenateData

---
+++ First steps in coding - Lab/T07.Concatenatedata/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace text_and_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = Console.ReadLine();
+            var lastname = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var town = Console.ReadLine();
+            Console.WriteLine("You are " + firstname + " " + lastname + ", a " + age + "-years old person from " + town + "."); ;
+        }
+    }
+}
