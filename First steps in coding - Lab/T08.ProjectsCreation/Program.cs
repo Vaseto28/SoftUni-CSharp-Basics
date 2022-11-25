70ee24dc6ea5e0a37df22a8c0c018b9e38bf220b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 11:56:21 +0200

    Completed task: T08.ProjectsCreation

---
+++ First steps in coding - Lab/T08.ProjectsCreation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projects_making
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var projects = int.Parse(Console.ReadLine());
+            var hours = projects * 3;
+            Console.WriteLine("The architect " + name + " will need " + hours + " hours to complete " + projects + " project/s.");
+        }
+    }
+}
