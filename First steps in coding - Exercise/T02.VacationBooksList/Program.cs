c0dae8398e592f3d4a27f6bc1b79e09b03076209
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 12:04:14 +0200

    Completed task: T02.VacationBooksList

---
+++ First steps in coding - Exercise/T02.VacationBooksList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace vaction_book_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pages = int.Parse(Console.ReadLine());
+            var pagesPerHour = int.Parse(Console.ReadLine());
+            var days = int.Parse(Console.ReadLine());
+            var neededDays = (pages / pagesPerHour) / days;
+            Console.WriteLine(neededDays);
+        }
+    }
+}
