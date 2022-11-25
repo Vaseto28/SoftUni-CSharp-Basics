dbe3ddf952f5bdda92b05def2eba11e7197dd0d8
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 12:04:38 +0200

    Completed task: T03.RadiansToDegrees

---
+++ First steps in coding - Exercise/T03.RadiansToDegrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace radians_to_degreeses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var radians = double.Parse(Console.ReadLine());
+            var degreeses = radians * (180 / Math.PI);
+            Console.WriteLine(degreeses);
+        }
+    }
+}
