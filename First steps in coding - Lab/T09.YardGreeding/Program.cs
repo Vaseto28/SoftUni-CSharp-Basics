3db11704c034d6910d1f9d17000221415442659d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 11:57:43 +0200

    Completed task: T09.YardGreeding

---
+++ First steps in coding - Lab/T09.YardGreeding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace yard_greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var area = double.Parse(Console.ReadLine());
+            var discount = (area * 7.61) * 0.18;
+            var finalprice = (area * 7.61) - discount;
+            Console.WriteLine("The final price is " + finalprice + " lv.");
+            Console.WriteLine("The discount is " + discount + " lv.");
+        }
+    }
+}
