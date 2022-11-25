988c93532a6790a858df6d3768932742e3d2f353
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 12:05:08 +0200

    Completed task: T04.DepositCalculator

---
+++ First steps in coding - Exercise/T04.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace deposit_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deposit = double.Parse(Console.ReadLine());
+            var period = int.Parse(Console.ReadLine());
+            var lihvenprocent = double.Parse(Console.ReadLine());
+            var suma = deposit + period * ((deposit * (lihvenprocent / 100)) / 12);
+            Console.WriteLine(suma);
+        }
+    }
+}
