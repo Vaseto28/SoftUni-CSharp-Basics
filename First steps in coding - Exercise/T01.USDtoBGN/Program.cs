f61b8e0ee9ec1c13c47e0cc2e091a3f417b1a003
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-25 12:03:25 +0200

    Completed task: T01.USDtoBGN

---
+++ First steps in coding - Exercise/T01.USDtoBGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var USD = double.Parse(Console.ReadLine());
+            var BGN = USD * 1.79549;
+            Console.WriteLine(BGN);
+        }
+    }
+}
