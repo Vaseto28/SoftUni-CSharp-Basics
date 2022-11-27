46b6e0c1ceea72357feb11a52b9bd726b0be77f1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 16:58:36 +0200

    Completed task: T06.FoodDelivery

---
+++ First steps in coding - Exercise/T07.FoodDelivery/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace food_delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chickenMenus = int.Parse(Console.ReadLine());
+            var fishMenus = int.Parse(Console.ReadLine());
+            var vegetableMenus = int.Parse(Console.ReadLine());
+            var priceOfMenus = chickenMenus * 10.35 + fishMenus * 12.40 + vegetableMenus * 8.15;
+            var finalprice = priceOfMenus + priceOfMenus * 0.20 + 2.50;
+            Console.WriteLine(finalprice);
+        }
+    }
+}
