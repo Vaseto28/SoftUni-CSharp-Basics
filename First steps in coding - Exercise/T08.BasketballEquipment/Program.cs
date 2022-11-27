6e95f765d5765b9c053184d540420e574e1215f0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 16:58:54 +0200

    Completed task: T08.BasketballEquipment

---
+++ First steps in coding - Exercise/T08.BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace basketball_equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tax = int.Parse(Console.ReadLine());
+            var shoes = tax - 0.40 * tax;
+            var clothes = shoes - 0.20 * shoes;
+            var ball = 0.25 * clothes;
+            var accessories = 0.20 * ball;
+            var finalprice = shoes + clothes + ball + accessories + tax;
+            Console.WriteLine(finalprice);
+        }
+    }
+}
