010550dfc1419d03ea2f4179632ca9ff6030e9a9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 17:01:00 +0200

    Completed task: T02.GraterNumber

---
+++ Conditional statements - Lab/T02.GraterNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bigger_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
