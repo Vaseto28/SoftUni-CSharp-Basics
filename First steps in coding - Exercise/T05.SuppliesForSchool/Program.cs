a204a760762c1817696e75d959149b544de08854
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 16:58:04 +0200

    Completed task: T05.SupplieForSchool

---
+++ First steps in coding - Exercise/T05.SuppliesForSchool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace supplies_for_school
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pens = int.Parse(Console.ReadLine());
+            var markers = int.Parse(Console.ReadLine());
+            var preparat = int.Parse(Console.ReadLine());
+            var procent = int.Parse(Console.ReadLine());
+            var suma = pens * 5.8 + markers * 7.2 + preparat * 1.2;
+            var finalPrice = suma - suma * procent / 100;
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
