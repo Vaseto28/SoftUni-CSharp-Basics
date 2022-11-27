565cc4daa05acfb5c1ff15775ad28cb5ca3a534f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 17:00:42 +0200

    Completed task: T01.ExcellentResult

---
+++ Conditional statements - Lab/T01.ExcellentResult/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace excellent_mark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mark = double.Parse(Console.ReadLine());
+            if (mark >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
