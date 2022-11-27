53b39449e0adecac3327e7d2b7b862c4a6f7f288
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 16:58:17 +0200

    Completed task: T06.Repainting

---
+++ First steps in coding - Exercise/T06.Repainting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var naylon = int.Parse(Console.ReadLine());
+            var boq = int.Parse(Console.ReadLine());
+            var razreditel = int.Parse(Console.ReadLine());
+            var hours = int.Parse(Console.ReadLine());
+            var price = ((naylon + 2) * 1.50) + ((boq + 0.1 * boq) * 14.50) + (razreditel * 5) + 0.40;
+            var workerPrice = hours * (price * 0.30);
+            var finalprice = price + workerPrice;
+            Console.WriteLine(finalprice);
+        }
+    }
+}
