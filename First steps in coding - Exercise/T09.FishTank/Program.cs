ccc23b62ed6406210afa9d8eeea0c82599b69abf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-27 16:59:10 +0200

    Completed task: T09.FishTank

---
+++ First steps in coding - Exercise/T09.FishTank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fish_tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lenght = int.Parse(Console.ReadLine());
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            var percent = double.Parse(Console.ReadLine());
+            double volume = (lenght * width * height) * 0.001;
+            var litres = volume - (percent / 100) * volume;
+            Console.WriteLine(litres);
+        }
+    }
+}
