2c357ba9c22510436571562fb94143109c0a788b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-28 15:09:16 +0200

    Completed task: T03.EvenOrOdd

---
+++ Conditional statements - Lab/T03.EvenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace even_or_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
