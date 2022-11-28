d5c3326b06a5ae93c3ee71e029bb033084cb9ccc
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-28 15:09:31 +0200

    Completed task: T04.PasswordGuess

---
+++ Conditional statements - Lab/T04.PasswordGuess/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace password_guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = Console.ReadLine();
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
