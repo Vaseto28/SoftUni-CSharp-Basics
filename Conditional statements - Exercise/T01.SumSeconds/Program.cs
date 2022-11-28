c7823d4703e3b9b1210d8afc2ea65995ace44b2f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-28 15:10:44 +0200

    Completed task: T01.SumSeconds

---
+++ Conditional statements - Exercise/T01.SumSeconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Seconds_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+            int totalTime = firstTime + secondTime + thirdTime;
+            int mins = totalTime / 60;
+            int secs = totalTime % 60;
+            if (secs < 10)
+            {
+                Console.WriteLine($"{mins}:{secs:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{mins}:{secs}");
+            }
+        }
+    }
+}
