7a74d415c7adae90af441a7b5bd2731d0f4bc8f9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-28 15:09:46 +0200

    Completed task: T05.NumberFrom100To200

---
+++ Conditional statements - Lab/T05.NumberFrom100To200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _100_to_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            if (a < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            if (a >= 100 && a <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            if (a > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
