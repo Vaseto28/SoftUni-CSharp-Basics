f25980b25bb4f458862b6e35538cf2e3d94cea4c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-28 15:10:23 +0200

    Completed task: T07.AreaOfFigures

---
+++ Conditional statements - Lab/T07.AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace area_of_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                var side = double.Parse(Console.ReadLine());
+                var area = side * side;
+                Console.WriteLine(area);
+            }
+            if (figure == "rectangle")
+            {
+                var lenght = double.Parse(Console.ReadLine());
+                var width = double.Parse(Console.ReadLine());
+                var area = lenght * width;
+                Console.WriteLine(area);
+            }
+            if (figure == "circle")
+            {
+                var radius = double.Parse(Console.ReadLine());
+                var area = radius * radius * Math.PI;
+                Console.WriteLine(area);
+            }
+            if (figure == "triangle")
+            {
+                var side = double.Parse(Console.ReadLine());
+                var height = double.Parse(Console.ReadLine());
+                var area = (side * height) / 2;
+                Console.WriteLine(area);
+            }
+        }
+    }
+}
