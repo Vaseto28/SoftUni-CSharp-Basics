7ea7fed2dfbcc1cef0892295a7e6ffe0f5456f30
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-29 09:05:24 +0200

    Completed task: T08.LunchBreak

---
+++ Conditional statements - Exercise/T08.LunchBreak/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lunch_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int episodeContinuous = int.Parse(Console.ReadLine());
+            int breakContinuous = int.Parse(Console.ReadLine());
+            double lunchAndRestTime = breakContinuous / 8.0 + breakContinuous / 4.0;
+            double timeLeft = breakContinuous - lunchAndRestTime;
+            if (timeLeft >= episodeContinuous)
+            {
+                double timeleft = Math.Abs(episodeContinuous - timeLeft);
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeleft)} minutes free time.");
+            }
+            else
+            {
+                double neededTime = Math.Abs(episodeContinuous - timeLeft);
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(neededTime)} more minutes.");
+            }
+        }
+    }
+}
