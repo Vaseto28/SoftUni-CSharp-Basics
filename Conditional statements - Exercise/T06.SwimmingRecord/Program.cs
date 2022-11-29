648c7ef37100be322999d459bef7a084c633e91e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-29 09:04:43 +0200

    Completed task: T06.SwimmingRecord

---
+++ Conditional statements - Exercise/T06.SwimmingRecord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace swimming_record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double speed = double.Parse(Console.ReadLine());
+            double time = meters * speed;
+            double delay = Math.Floor(meters / 15) * 12.5;
+            double totalTime = time + delay;
+            if (totalTime >= record)
+            {
+                double timeLeft = Math.Abs(record - totalTime);
+                Console.WriteLine($"No, he failed! He was {timeLeft:f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+        }
+    }
+}
