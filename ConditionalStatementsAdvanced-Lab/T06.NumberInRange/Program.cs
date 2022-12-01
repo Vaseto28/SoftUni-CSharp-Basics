1077a85f0f690d4dc895c57129b903fd27392157
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:16:13 +0200

    Completed task: T06.NumberInRange

---
+++ ConditionalStatementsAdvanced-Lab/T06.NumberInRange/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace number_in_range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else if (number >= -100 && number <= 100)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
