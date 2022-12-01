11106994c7e366e8540dfca14eeb91959c0bf63b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:17:05 +0200

    Completed task: T09.FruitOrVegetable

---
+++ ConditionalStatementsAdvanced-Lab/T09.FruitOrVegetable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fruit_or_vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = Console.ReadLine();
+            switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
