d02752694c651905e2acdfb8a0c21d0261a75d29
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:15:54 +0200

    Completed task: T05.SmallShop

---
+++ ConditionalStatementsAdvanced-Lab/T05.SmallShop/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace neighbourhood_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = Console.ReadLine();
+            var city = Console.ReadLine();
+            var value = double.Parse(Console.ReadLine());
+            var price = 0.0;
+            var finalPrice = 0.0;
+            switch (city)
+            {
+                case "Sofia":
+                    switch (product)
+                    {
+                        case "coffee":
+                            price = 0.50;
+                            break;
+                        case "water":
+                            price = 0.80;
+                            break;
+                        case "beer":
+                            price = 1.20;
+                            break;
+                        case "sweets":
+                            price = 1.45;
+                            break;
+                        case "peanuts":
+                            price = 1.60;
+                            break;
+                    }
+                    break;
+
+                case "Plovdiv":
+                    switch (product)
+                    {
+                        case "coffee":
+                            price = 0.40;
+                            break;
+                        case "water":
+                            price = 0.70;
+                            break;
+                        case "beer":
+                            price = 1.15;
+                            break;
+                        case "sweets":
+                            price = 1.30;
+                            break;
+                        case "peanuts":
+                            price = 1.50;
+                            break;
+                    }
+                    break;
+
+                case "Varna":
+                    switch (product)
+                    {
+                        case "coffee":
+                            price = 0.45;
+                            break;
+                        case "water":
+                            price = 0.70;
+                            break;
+                        case "beer":
+                            price = 1.10;
+                            break;
+                        case "sweets":
+                            price = 1.35;
+                            break;
+                        case "peanuts":
+                            price = 1.55;
+                            break;
+                    }
+                    break;
+            }
+            finalPrice = value * price;
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
