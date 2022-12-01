c3a566b985d95e9709a813c87c1b848d4a23f5ed
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:16:51 +0200

    Completed task: T08.CinemaTicket

---
+++ ConditionalStatementsAdvanced-Lab/T08.CinemaTicket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cinema_ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayFromWeek = Console.ReadLine();
+            var ticketPrice = 0;
+            switch (dayFromWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    ticketPrice = 12;
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    ticketPrice = 14;
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    ticketPrice = 16;
+                    break;
+            }
+            Console.WriteLine(ticketPrice);
+        }
+    }
+}
