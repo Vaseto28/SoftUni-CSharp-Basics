f2755a470bac7561f836884c7b8eef2d57778735
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:16:31 +0200

    Completed task: T07.WorkingHours

---
+++ ConditionalStatementsAdvanced-Lab/T07.WorkingHours/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace working_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var dayFromWeek = Console.ReadLine();
+            switch (dayFromWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    if (hour >= 10 && hour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Sunday":
+                    Console.WriteLine("closed");
+                    break;
+            }
+        }
+    }
+}
