3d717cae482216aa048e9b0276091743234fad3a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:15:30 +0200

    Completed task: T03.AnimalType

---
+++ ConditionalStatementsAdvanced-Lab/T03.AnimalType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace animal_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animalName = Console.ReadLine();
+            switch (animalName)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
