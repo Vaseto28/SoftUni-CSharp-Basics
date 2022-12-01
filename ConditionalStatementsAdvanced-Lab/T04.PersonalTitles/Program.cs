ac4471ff83ea96e9a2d74a7d764456fc1368657d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-01 10:15:43 +0200

    Completed task: T04.PersonalTitles

---
+++ ConditionalStatementsAdvanced-Lab/T04.PersonalTitles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace personal_titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            if (gender == 'f')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else if (gender == 'm')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+        }
+    }
+}
