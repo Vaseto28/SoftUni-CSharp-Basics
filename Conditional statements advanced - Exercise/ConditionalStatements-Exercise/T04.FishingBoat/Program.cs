e060c4bd498c9bb72110dcae09f637da6c493802
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-02 16:45:09 +0200

    Completed rask: T04.FishingBoat

---
+++ Conditional statements advanced - Exercise/ConditionalStatements-Exercise/T04.FishingBoat/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace fishing_boat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = int.Parse(Console.ReadLine());
+            var season = Console.ReadLine();
+            var fishermen = int.Parse(Console.ReadLine());
+            var finalPrice = 0.0;
+            switch (season)
+            {
+                case "Spring":
+                    finalPrice = 3000;
+                    if (fishermen <= 6)
+                    {
+                        finalPrice *= 0.90;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        finalPrice *= 0.85;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        finalPrice *= 0.75;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        finalPrice = finalPrice - 0.05 * finalPrice;
+                    }
+                    break;
+                case "Summer":
+                    finalPrice = 4200;
+                    if (fishermen <= 6)
+                    {
+                        finalPrice *= 0.90;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        finalPrice *= 0.85;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        finalPrice *= 0.75;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        finalPrice = finalPrice - 0.05 * finalPrice;
+                    }
+                    break;
+                case "Autumn":
+                    finalPrice = 4200;
+                    if (fishermen <= 6)
+                    {
+                        finalPrice *= 0.90;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        finalPrice *= 0.85;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        finalPrice *= 0.75;
+                    }
+                    break;
+                case "Winter":
+                    finalPrice = 2600;
+                    if (fishermen <= 6)
+                    {
+                        finalPrice *= 0.90;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        finalPrice *= 0.85;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        finalPrice *= 0.75;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        finalPrice = finalPrice - 0.05 * finalPrice;
+                    }
+                    break;
+            }
+            if (budget >= finalPrice)
+            {
+                var moneyLeft = Math.Abs(budget - finalPrice);
+                Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+            }
+            else
+            {
+                var neededMoney = Math.Abs(budget - finalPrice);
+                Console.WriteLine($"Not enough money! You need {neededMoney:f2} leva.");
+            }
+        }
+    }
+}
