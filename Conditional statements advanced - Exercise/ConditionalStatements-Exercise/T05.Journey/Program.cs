21b35fd0ed417f9ead998a7486b52f862e04f4cc
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-02 16:45:20 +0200

    Completed task: T05.Journey

---
+++ Conditional statements advanced - Exercise/ConditionalStatements-Exercise/T05.Journey/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = double.Parse(Console.ReadLine());
+            var season = Console.ReadLine();
+            var finalPrice = 0.0;
+            switch (season)
+            {
+                case "summer":
+                    if (budget <= 100)
+                    {
+                        finalPrice = budget * 0.30;
+                    }
+                    else if (budget > 100 && budget <= 1000)
+                    {
+                        finalPrice = budget * 0.40;
+                    }
+                    else
+                    {
+                        finalPrice = budget * 0.90;
+                    }
+                    break;
+                case "winter":
+                    if (budget <= 100)
+                    {
+                        finalPrice = budget * 0.70;
+                    }
+                    else if (budget > 100 && budget <= 1000)
+                    {
+                        finalPrice = budget * 0.80;
+                    }
+                    else
+                    {
+                        finalPrice = budget * 0.90;
+                    }
+                    break;
+            }
+            if (budget <= 100)
+            {
+                Console.WriteLine("Somewhere in Bulgaria");
+                if (season == "summer")
+                {
+                    Console.WriteLine($"Camp - {finalPrice:f2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Hotel - {finalPrice:f2}");
+                }
+            }
+            else if (budget > 100 && budget <= 1000)
+            {
+                Console.WriteLine("Somewhere in Balkans");
+                if (season == "summer")
+                {
+                    Console.WriteLine($"Camp - {finalPrice:f2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Hotel - {finalPrice:f2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Somewhere in Europe");
+                Console.WriteLine($"Hotel - {finalPrice:f2}");
+            }
+        }
+    }
+}
