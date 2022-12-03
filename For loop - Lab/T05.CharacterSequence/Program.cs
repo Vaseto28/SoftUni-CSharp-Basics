317c29aff8a64dd5343a411e801579336dee543b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-03 17:59:21 +0200

    Completed task: T05.CharacterSequence

---
+++ For loop - Lab/T05.CharacterSequence/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace character_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
