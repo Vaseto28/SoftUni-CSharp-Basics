888bbca474052ca6742627b2194893c506da0b14
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-03 17:59:58 +0200

    Completed task: T07.SumNumbers

---
+++ For loop - Lab/T07.SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sum_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                var numbers = int.Parse(Console.ReadLine());
+                sum += numbers;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
