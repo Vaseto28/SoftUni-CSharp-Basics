0abb65c55b4f0bae46539f965ff7a3912729d3f0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-03 17:59:06 +0200

    Completed task: T04.EvenPowersOfN

---
+++ For loop - Lab/T04.EvenPowersOfN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace even_powers_of_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powers = int.Parse(Console.ReadLine());
+            var num = 1;
+            for (int i = 0; i <= powers; i += 2)
+            {
+                Console.WriteLine(num);
+                num = 4 * num;
+            }
+        }
+    }
+}
