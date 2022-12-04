a37928cd95da7d1de13b6585553076dfae9ddf1d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-04 22:55:59 +0200

    Completed task: T02.HalfSumElement

---
+++ For loop - Exercise/T02.HalfSumElement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace half_sum_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var max = int.MinValue;
+            for (int i = 0; i < count; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                if (number >= max)
+                {
+                    max = number;
+                }
+            }
+            sum = sum - max;
+            if (sum == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum - max)}");
+            }
+        }
+    }
+}
