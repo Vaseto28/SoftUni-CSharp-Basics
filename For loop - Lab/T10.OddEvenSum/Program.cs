ee7f067975d67d8dd9b38ac57d2a0b8d5ac58523
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-04 22:55:21 +0200

    Completed task: T10.OddEvenSum

---
+++ For loop - Lab/T10.OddEvenSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace odd_or_even_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var oddSum = 0;
+            var evenSum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + number;
+                }
+                else
+                {
+                    oddSum = oddSum + number;
+                }
+            }
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum - evenSum)}");
+            }
+        }
+    }
+}
