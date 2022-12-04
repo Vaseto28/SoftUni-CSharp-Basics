be3d1ad38e8ff8f13f5284dfa7acb9663d026946
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-04 22:56:26 +0200

    Completed task: T04.CleverLily

---
+++ For loop - Exercise/T04.CleverLily/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = int.Parse(Console.ReadLine());
+            var washingMachinePrice = double.Parse(Console.ReadLine());
+            var toyPrice = int.Parse(Console.ReadLine());
+            var toysCount = 0;
+            var sum = 0;
+            var totalSaves = 0;
+            var saves = 0;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = (i * 5) - 1;
+                    saves = saves + sum;
+                }
+                else
+                {
+                    toysCount += 1;
+                }
+            }
+            totalSaves = saves + (toysCount * toyPrice);
+            if (totalSaves >= washingMachinePrice)
+            {
+                var moneyLeft = Math.Abs(totalSaves - washingMachinePrice);
+                Console.WriteLine($"Yes! {moneyLeft:f2}");
+            }
+            else
+            {
+                var neededMoney = Math.Abs(totalSaves - washingMachinePrice);
+                Console.WriteLine($"No! {neededMoney:f2}");
+            }
+        }
+    }
+}
