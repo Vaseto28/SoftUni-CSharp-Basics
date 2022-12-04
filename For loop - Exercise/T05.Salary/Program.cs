3999271187f4107d2c2dd6e68d1f316594ed84dd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-04 22:56:39 +0200

    Completed task: T05.Salary

---
+++ For loop - Exercise/T05.Salary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tabsCount = int.Parse(Console.ReadLine());
+            var salary = double.Parse(Console.ReadLine());
+            var fine = 0;
+            for (int i = 0; i < tabsCount; i++)
+            {
+                var tabs = Console.ReadLine();
+                switch (tabs)
+                {
+                    case "Facebook":
+                        fine += 150;
+                        break;
+                    case "Instagram":
+                        fine += 100;
+                        break;
+                    case "Reddit":
+                        fine += 50;
+                        break;
+                }
+                if (fine >= salary)
+                {
+                    break;
+                }
+            }
+            if (fine >= salary)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(Math.Abs(fine - salary));
+            }
+        }
+    }
+}
