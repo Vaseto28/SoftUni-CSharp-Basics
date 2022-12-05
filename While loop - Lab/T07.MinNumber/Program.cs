570d08b68cca4842f6c3bb26a56254996614525a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:01:40 +0200

    Complete task: T07.MinNumber

---
+++ While loop - Lab/T07.MinNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace max_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = Console.ReadLine();
+            var minNum = int.MaxValue;
+            while (commands != "Stop")
+            {
+                var num = int.Parse(commands);
+                if (num < minNum)
+                {
+                    minNum = num;
+                }
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
