cd0722b55b58326437fe63dcb3e2d5e6eae9c8a0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:00:58 +0200

    Completed task: T04.Sequence2kPlusOne

---
+++ While loop - Lab/T04.Sequence2kPlusOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var newNum = 1;
+            while (newNum <= num)
+            {
+                Console.WriteLine(newNum);
+                newNum = 2 * newNum + 1;
+            }
+
+        }
+    }
+}
