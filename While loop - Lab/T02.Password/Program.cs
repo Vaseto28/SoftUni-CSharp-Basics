75c0165e0080514eb664bebe8d6daf739dc119b3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:00:18 +0200

    Completed task: T02.Password

---
+++ While loop - Lab/T02.Password/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = Console.ReadLine();
+            var password = Console.ReadLine();
+            var inputPassowrd = Console.ReadLine();
+            while (inputPassowrd != password)
+            {
+                inputPassowrd = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {username}!");
+        }
+    }
+}
