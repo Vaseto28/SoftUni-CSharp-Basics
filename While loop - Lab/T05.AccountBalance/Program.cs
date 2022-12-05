a9390d66d9220918141cbd5c976c1f2eeb5679b3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:01:11 +0200

    Completed task: T05.AccountBalance

---
+++ While loop - Lab/T05.AccountBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace account_money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comands = Console.ReadLine();
+            var sum = 0.0;
+            while (comands != "NoMoreMoney")
+            {
+                var amount = double.Parse(comands);
+                if (amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum += amount;
+                Console.WriteLine($"Increase: {amount:f2}");
+                comands = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
