82acb7edcd25f398d94311fa7fc8caaf68642ef1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:01:25 +0200

    Completed task: T06.MaxNumber

---
+++ While loop - Lab/T06.MaxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace max_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = Console.ReadLine();
+            var maxNum = int.MinValue;
+            while (commands != "Stop")
+            {
+                var num = int.Parse(commands);
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
