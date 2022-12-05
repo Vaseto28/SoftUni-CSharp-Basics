f165dfe757b250e0e5df9495e148c8c9ae64424a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:00:05 +0200

    Completed task: T01.ReadText

---
+++ While loop - Lab/T01.ReadText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Read_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            while (text != "Stop")
+            {
+                Console.WriteLine(text);
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
