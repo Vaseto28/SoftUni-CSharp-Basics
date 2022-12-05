6831f7d929f85e6fa1aa524bd56e201938d4221b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-05 12:00:36 +0200

    Completed task: T03.SumOfNumbers

---
+++ While loop - Lab/T03.SumOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sum_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = int.Parse(Console.ReadLine());
+            var sum1 = 0;
+            while (sum > sum1)
+            {
+                var num = int.Parse(Console.ReadLine());
+                sum1 += num;
+            }
+            Console.WriteLine(sum1);
+        }
+    }
+}
