10679c0506c125397c0cb3c53281e9b2f122b63a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-06 08:08:29 +0200

    Completed task: T06.Cakes

---
+++ While loop - Exercise/T06.Cakes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = int.Parse(Console.ReadLine());
+            var lenght = int.Parse(Console.ReadLine());
+            var commands = Console.ReadLine();
+            var area = width * lenght;
+            while (commands != "STOP")
+            {
+                var piece = int.Parse(commands);
+                area -= piece;
+                if (area < 0)
+                {
+                    var morePieces = Math.Abs(area);
+                    Console.WriteLine($"No more cake left! You need {morePieces} pieces more.");
+                    break;
+                }
+                commands = Console.ReadLine();
+            }
+            if (commands == "STOP")
+            {
+                var piecesLeft = Math.Abs(area);
+                Console.WriteLine($"{piecesLeft} pieces are left.");
+            }
+        }
+    }
+}
