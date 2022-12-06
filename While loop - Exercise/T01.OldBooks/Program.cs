5aeacceccee2ee5af2d1ecbab73b9c0c058ec0c4
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-06 08:06:56 +0200

    Completed task: T01.OldBooks

---
+++ While loop - Exercise/T01.OldBooks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace old_books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favouriteBook = Console.ReadLine();
+            var book = Console.ReadLine();
+            var count = 0;
+            while (book != "No More Books")
+            {
+                if (book == favouriteBook)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    break;
+                }
+                book = Console.ReadLine();
+                count++;
+            }
+            if (book == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+            }
+        }
+    }
+}
