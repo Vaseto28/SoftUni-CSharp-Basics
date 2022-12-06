3ac984d561d4f4b1cb0c9b252b092a8af1a1351a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-06 08:08:47 +0200

    Completed task: T07.Moving

---
+++ While loop - Exercise/T07.Mooving/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mooving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = int.Parse(Console.ReadLine());
+            var lenght = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            var capacity = width * lenght * height;
+            var commands = Console.ReadLine();
+            while (commands != "Done")
+            {
+                var boxCapacity = int.Parse(commands);
+                capacity -= boxCapacity;
+                if (capacity < 0)
+                {
+                    var neededCubicMeters = Math.Abs(capacity);
+                    Console.WriteLine($"No more free space! You need {neededCubicMeters} Cubic meters more.");
+                    return;
+                }
+                commands = Console.ReadLine();
+            }
+            var moreCubicMeters = Math.Abs(capacity);
+            Console.WriteLine($"{moreCubicMeters} Cubic meters left.");
+        }
+    }
+}
