f85fa473ac5069db947019760ac80a8c968a1f0a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-06 08:08:02 +0200

    Completed task: T04.Walking

---
+++ While loop - Exercise/T04.Walking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = Console.ReadLine();
+            var steps = 0;
+            var totalSteps = 0;
+            while (commands != "Going home")
+            {
+                steps = int.Parse(commands);
+                totalSteps += steps;
+                if (totalSteps >= 10000)
+                {
+                    break;
+                }
+                commands = Console.ReadLine();
+            }
+            if (commands == "Going home")
+            {
+                commands = Console.ReadLine();
+                steps = int.Parse(commands);
+                totalSteps += steps;
+            }
+            var difference = Math.Abs(totalSteps - 10000);
+            if (totalSteps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{difference} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{difference} more steps to reach goal.");
+            }
+        }
+    }
+}
