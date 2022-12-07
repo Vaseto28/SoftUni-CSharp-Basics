18aeddd729ac783fdda08ec89741fce52616db98
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:38:07 +0200

    Completed task: T03.Combinations

---
+++ Nasted loops - Lab/T03.Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salutions = int.Parse(Console.ReadLine());
+            var validCombinations = 0;
+            for (int i1 = 0; i1 <= salutions; i1++)
+            {
+                for (int i2 = 0; i2 <= salutions; i2++)
+                {
+                    for (int i3 = 0; i3 <= salutions; i3++)
+                    {
+                        if (i1 + i2 + i3 == salutions)
+                        {
+                            validCombinations++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombinations);
+        }
+    }
+}
