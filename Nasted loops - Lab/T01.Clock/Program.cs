4608541fca69b47e3eab4d353e3b0bd1a8e6e40e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:37:06 +0200

    Completed task: T01.Clock

---
+++ Nasted loops - Lab/T01.Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                for (int mins = 0; mins < 60; mins++)
+                {
+                    Console.WriteLine($"{hour}:{mins}");
+                }
+            }
+        }
+    }
+}
