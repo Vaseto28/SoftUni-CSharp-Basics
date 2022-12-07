a9254d0a1fe70d0e26f1249734a144cd7b2b4edf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:39:03 +0200

    Completed task: T06.Building

---
+++ Nasted loops - Lab/T06.Building/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var floors = int.Parse(Console.ReadLine());
+            var rooms = int.Parse(Console.ReadLine());
+            for (int floor = floors; floor > 0; floor--)
+            {
+                if (floor == floors)
+                {
+                    for (int room = 0; room < rooms; room++)
+                    {
+                        Console.Write($"L{floor}{room} ");
+                    }
+                    Console.WriteLine();
+                }
+                else if (floor % 2 == 0)
+                {
+                    for (int room = 0; room < rooms; room++)
+                    {
+                        Console.Write($"O{floor}{room} ");
+                    }
+                    Console.WriteLine();
+                }
+                else if (floor % 2 != 0)
+                {
+                    for (int room = 0; room < rooms; room++)
+                    {
+                        Console.Write($"A{floor}{room} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
