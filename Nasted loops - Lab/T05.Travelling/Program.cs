fd967b54ed33357ab63ea97cee4271773a77f00f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:38:48 +0200

    Completed task: T05.Travelling

---
+++ Nasted loops - Lab/T05.Travelling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                var budget = double.Parse(Console.ReadLine());
+                var savings = 0.0;
+                while (savings < budget)
+                {
+                    savings += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
