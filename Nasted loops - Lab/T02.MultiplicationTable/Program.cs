4cf24b107492c7dcfe479ee9253f399d0d88c020
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:37:52 +0200

    Completed task: T02.MultiplicationTable

---
+++ Nasted loops - Lab/T02.MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 11; a++)
+            {
+                for (int b = 1; b < 11; b++)
+                {
+                    Console.WriteLine($"{a} * {b} = {a * b}");
+                }
+            }
+        }
+    }
+}
