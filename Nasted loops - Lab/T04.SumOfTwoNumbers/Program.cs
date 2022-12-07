baa07f2e86a0837ea162ea13a865c71f5b699aa5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:38:25 +0200

    Completed task: T04.SumOfTwoNumbers

---
+++ Nasted loops - Lab/T04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sum_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intervalStart = int.Parse(Console.ReadLine());
+            var intervalEnd = int.Parse(Console.ReadLine());
+            var magicNumber = int.Parse(Console.ReadLine());
+            var counter = 0;
+            for (int firstNumber = intervalStart; firstNumber <= intervalEnd; firstNumber++)
+            {
+                for (int secondNumber = intervalStart; secondNumber <= intervalEnd; secondNumber++)
+                {
+                    var sum = firstNumber + secondNumber;
+                    counter++;
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({firstNumber} + {secondNumber} = {magicNumber})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+        }
+    }
+}
