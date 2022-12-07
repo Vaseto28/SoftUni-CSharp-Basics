37792a58742d7cc4d45aed8fcc466121cb642e0e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-07 14:39:21 +0200

    Completed task: T01.NumberPyramid

---
+++ Nasted loops - Exercise/T01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace number_pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var current = 1;
+            var isBigger = false;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write($"{current} ");
+                    current++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
